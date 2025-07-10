7dd8b7f8e5c33f8cd3a6c953d27956ebc28c5adc
mkhotamirais <mkhotamirais@gmail.com>
2025-07-10 20:34:42 +0700

    update client page

--- app/(root)/clients/page.tsx
+++ app/(root)/clients/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Heroes from "../(home-components)/Heroes";
-// import Image from "next/image";
-// import { clientPhotos } from "../content";
+import Image from "next/image";
+import { clientPhotos } from "../content";
 
 export default function Clients() {
   return (
@@ -10,7 +10,7 @@ export default function Clients() {
       <section className="py-16 bg-white text-gray-800">
         <div className="container">
           <div className="grid grid-cols-3 md:grid-cols-6 gap-8">
-            {/* {clientPhotos.map((item, index) => (
+            {clientPhotos.map((item, index) => (
               <Image
                 key={index}
                 src={item}
@@ -20,7 +20,7 @@ export default function Clients() {
                 className="bg-white rounded-lg object-contain object-center h-24 w-full"
                 priority
               />
-            ))} */}
+            ))}
           </div>
         </div>
       </section>
